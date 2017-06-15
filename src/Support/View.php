a87e519ba3c85cfdd1ef6e4d96197a29c70963b2
Pascal Kleindienst <mail@pascalkleindienst.de>
2017-06-15 16:17:13 +0200

    simple view class

---
+++ src/Support/View.php
@@ -0,0 +1,71 @@
+<?php namespace PascalKleindienst\FormListGenerator\Support;
+
+/**
+ * Simple View class
+ * @package \PascalKleindienst\FormListGenerator\Support
+ */
+class View
+{
+    /**
+     * @var null|string
+     */
+    protected $path;
+
+    /**
+     * @var null|string
+     */
+    protected $extension;
+
+    /**
+     * Set params
+     *
+     * @param string $path
+     * @param string $extension
+     */
+    public function __construct($path = '', $extension = '.phtml')
+    {
+        // default view path
+        $this->path = realpath(__DIR__ . '/../views/');
+
+        // custom view path
+        if ($path !== '') {
+            $this->path = str_replace('~', Config::get('root'), $path);
+        }
+        
+        // Extension
+        $this->extension = $extension;
+    }
+
+    /**
+     * Load a view file
+     *
+     * @param string $file
+     * @param array $data
+     * @return void
+     * @throws \InvalidArgumentException if the file could not be found
+     */
+    public function render($file, array $data = [])
+    {   
+        // Check if file exists
+        $fullpath = $this->getFileName($file);
+        
+        if (!file_exists($fullpath)) {
+            throw new \InvalidArgumentException('Could not find file with name ' . $file);
+        }
+
+        // load file
+        extract($data);
+        include_once($fullpath);
+    }
+
+    /**
+     * Get the full file name
+     *
+     * @param string $file
+     * @return string
+     */
+    protected function getFileName($file)
+    {
+        return $this->path . '/_' . $file . $this->extension;
+    }
+}
