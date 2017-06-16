fb3b7efdf29a5b887f9ad67e08cb8c70e525046b
Pascal Kleindienst <mail@pascalkleindienst.de>
2017-06-16 10:05:49 +0200

    added column class + tests

---
+++ src/Data/Column.php
@@ -0,0 +1,148 @@
+<?php namespace PascalKleindienst\FormListGenerator\Data;
+
+use PascalKleindienst\FormListGenerator\Support\Config;
+
+/**
+ * Columns for list generator
+ * @package \PascalKleindienst\FormListGenerator\Data
+ */
+class Column
+{
+    /**
+     * @var string List column name.
+     */
+    public $columnName;
+
+    /**
+     * @var string List column label.
+     */
+    public $label;
+
+    /**
+     * @var string Display mode. Text, number
+     */
+    public $type = 'text';
+
+    /**
+     * @var boolean Specifies whether a column is clickable or not
+     */
+    public $clickable = false;
+
+    /**
+     * @var string Specifies a default value when value is empty.
+     */
+    public $default;
+
+    /**
+     * @var string Specify a CSS class to attach to the list cell element.
+     */
+    public $cssClass;
+
+    /**
+     * @var string Specify a format or style for the column value, such as a Date.
+     */
+    public $format;
+
+    /**
+     * @var string Specifies a path for partial-type fields.
+     */
+    public $path;
+
+    /**
+     * @var array Raw field configuration.
+     */
+    public $config;
+
+    /**
+     * @var string Record Url
+     */
+    protected $recordUrl;
+
+    /**
+     * Constructor.
+     * @param string $columnName
+     * @param string $label
+     */
+    public function __construct($name, $config, $recordUrl)
+    {
+        $this->columnName = $name;
+        $this->config = $config;
+        $this->recordUrl = $recordUrl;
+        $this->setup();
+    }
+
+    /**
+     * Setup the column properties
+     *
+     * @return void
+     */
+    protected function setup()
+    {
+        // type
+        $this->type = isset($this->config['type']) ? strtolower($this->config['type']) : 'text';
+
+        // save value of properties if they exist
+        $configKeys = ['cssClass', 'default', 'format', 'path', 'label', 'clickable'];
+
+        foreach ($configKeys as $key) {
+            if (isset($this->config[$key])) {
+                $this->{$key} = $this->config[$key];
+            }
+        }
+        
+        // save path
+        $this->path = str_replace('~', Config::get('root'), $this->path);
+    }
+
+    /**
+     * Get the value for the column from the record
+     *
+     * @param array $record
+     * @return void|string
+     */
+    public function getValue(array $record)
+    {
+        // get value or default value if record does not exist
+        $value = $this->default;
+        if (array_key_exists($this->columnName, $record)) {
+            $value = $record[$this->columnName];
+        }
+        
+        // format date
+        if ($this->type === 'date') {
+            $value = date($this->format, $value);
+        }
+
+        // format number
+        if ($this->type === 'number') {
+            $value = '<div class="text-right">' . $value . '</div>';
+        }
+        
+        // load partial
+        if ($this->type === 'partial' && file_exists($this->path)) {
+            include($this->path);
+            return;
+        }
+
+        return $value;
+    }
+
+    /**
+     * Get the record url
+     *
+     * @param array $record
+     * @return string
+     */
+    public function getRecordUrl(array $record)
+    {
+        // search patterns ala :id basend on record fields
+        $search = array_map(
+            function ($value) {
+                return ":$value";
+            },
+            array_keys($record)
+        );
+        
+        return str_replace($search, $record, $this->recordUrl);
+    }
+}
---
+++ tests/Data/ColumnTest.php
@@ -0,0 +1,109 @@
+<?php namespace PascalKleindienst\FormListGenerator\Data;
+
+/**
+ * Column Test Class
+ * @package \PascalKleindienst\FormListGenerator\Data
+ */
+class ColumnTest extends \PHPUnit_Framework_TestCase
+{
+    public function testConstructor()
+    {
+        $col = new Column('test', [], '');
+        $this->assertEquals('test', $col->columnName);
+        $this->assertEmpty($col->config);
+    }
+
+    public function testGetRecordUrl()
+    {
+        $col = new Column('test', [], '/foo/bar/:baz');
+        $this->assertEquals('/foo/bar/42', $col->getRecordUrl(['baz' => '42']));
+    }
+
+    public function testSetupEmptyConfig()
+    {
+        $col = new Column('', [], '');
+        
+        $this->assertEquals('text', $col->type);
+        $this->assertFalse($col->clickable);
+        $this->assertEquals('', $col->columnName, 'Empty Column Name');
+        $this->assertEquals('', $col->path, 'Empty path');
+        $this->assertNull($col->label, 'Label not set');
+        $this->assertNull($col->default, 'Default not set');
+        $this->assertNull($col->cssClass, 'cssClass not set');
+        $this->assertNull($col->format, 'Format not set');
+        $this->assertEmpty($col->config, 'Empty Config');
+    }
+
+    public function testSetupWithConfig()
+    {
+        $config = [
+            'type'      => 'number',
+            'clickable' => true,
+            'label'     => 'I am a label',
+            'default'   => 'Default value',
+            'cssClass'  => 'my-awesome-class my-other-awesome-class',
+            'format'    => 'd.m.Y',
+            'path'      => '../views/'
+        ];
+        $col = new Column('', $config, '');
+        
+        $this->assertEquals($config['type'], $col->type);
+        $this->assertEquals($config['clickable'], $col->clickable);
+        $this->assertEquals($config['label'], $col->label);
+        $this->assertEquals($config['default'], $col->default);
+        $this->assertEquals($config['cssClass'], $col->cssClass);
+        $this->assertEquals($config['format'], $col->format);
+        $this->assertEquals($config['path'], $col->path);
+    }
+    
+    /**
+     * @dataProvider provider
+     */
+    public function testGetValue($column, $config, $expected)
+    {
+        $record =  [
+            'full_name'  => 'John Doe',
+            'age'        => 42,
+            'created_at' => time(),
+            'content'    => 'lorem ipsum',
+            'default'    => null
+        ];
+        $col = new Column($column, $config, '');
+        
+        $this->assertEquals($expected($record[$column]), $col->getValue($record));
+    }
+
+    public function testGetDefaultValue()
+    {
+        $record =  [];
+        $col = new Column('default', ['default' => 42], '');
+        
+        $this->assertEquals(42, $col->getValue($record));
+    }
+
+    public function testGetPartialValue()
+    {
+        ob_start();
+        $record =  ['content' => 'lorem ipsum'];
+        $col = new Column('content', ['type' => 'partial', 'path' => __DIR__ . '/../views/' . '_col_partial.phtml'], '');
+        
+        $col->getValue($record);
+        $this->assertEquals('Col Partial ' . $record['content'], ob_get_contents());
+        ob_end_clean();
+    }
+
+    public function provider()
+    {
+        return [
+           'text column'   => ['full_name', ['type' => 'text'], function ($record) {
+               return $record;
+           }],
+           'number column' => ['age', ['type' => 'number'], function ($record) {
+               return '<div class="text-right">' . $record . '</div>';
+           }],
+           'date column'   => ['created_at', ['type' => 'date', 'format' => 'd.m.Y'], function ($record) {
+               return date('d.m.Y', $record);
+           }]
+        ];
+    }
+}
