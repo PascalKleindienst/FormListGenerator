d24b358d6e625917a9cb684505abcf9a5f8bde53
Pascal Kleindienst <mail@pascalkleindienst.de>
2017-07-17 09:29:25 +0200

    added class for default fields like text, password etc

---
+++ src/Fields/Field.php
@@ -0,0 +1,118 @@
+<?php namespace PascalKleindienst\FormListGenerator\Fields;
+
+/**
+ * Field for Form generator
+ * @package \PascalKleindienst\FormListGenerator\Fields
+ */
+class Field extends AbstractField
+{
+    /**
+     * @var string Specify a placeholder for the input
+     */
+    public $placeholder = '';
+
+    /**
+     * @var array Options used for a select tag
+     */
+    public $options = [];
+
+    /**
+     * @var mixed Default value for the input element
+     */
+    public $default;
+
+    /**
+     * {@inheritDoc}
+     */
+    protected function registerConfigKeys()
+    {
+        return [ 'placeholder', 'options' ];
+    }
+
+    /**
+     * {@inheritDoc}
+     */
+    public function getValue(array $records = [])
+    {
+        // Types
+        $this->initInputType($this->getRecord($records));
+        
+        // Attributes
+        $this->setAttributes();
+
+        // Disabled
+        if ($this->disabled) {
+            $this->input->disable();
+        }
+
+        // Readonly
+        if ($this->readOnly) {
+            $this->input->attribute('readonly', true);
+        }
+
+        // required
+        $this->setRequired(' *');
+
+        // Placeholder
+        if ($this->placeholder !== '') {
+            $this->input->placeholder($this->placeholder);
+        }
+        
+        // Label
+        if (isset($this->config['comment'])) {
+            $this->cssClass = str_replace('form-control', '', $this->cssClass);
+            $this->input = $this->labelAfterInput(' ' . $this->config['comment']);
+        }
+
+        // Css
+        $this->input->addClass($this->cssClass);
+
+        return $this->builder->label($this->label)->addClass('d-block')->before($this->input);
+    }
+
+    /**
+     * Initialize the input field based on the type
+     *
+     * @param mixed $record
+     * @return void
+     */
+    protected function initInputType($record)
+    {
+        // init input
+        switch ($this->type) {
+            case 'password':
+                $this->input = $this->builder->password($this->fieldName);
+                break;
+            case 'date':
+                $this->input = $this->builder->date($this->fieldName);
+                break;
+            case 'email':
+                $this->input = $this->builder->email($this->fieldName);
+                break;
+            case 'textarea':
+                $this->input = $this->builder->textarea($this->fieldName);
+                break;
+            case 'dropdown':
+                $this->input = $this->builder->select($this->fieldName, $this->getOptions())->select($record);
+                break;
+            case 'checkbox':
+                $this->input = $this->builder->checkbox($this->fieldName);
+
+                if ($this->default) {
+                    $this->input->check();
+                }
+                
+                $this->input->value($record);
+                break;
+            default:
+                $this->input = $this->builder->text($this->fieldName);
+        }
+
+        // set value and default value
+        if ($this->type === 'date') {
+            $this->input->value(new \DateTime($record));
+        } elseif ($this->type !== 'dropdown' && $this->type !== 'checkbox') {
+            $this->input->value($record)->defaultValue($this->default);
+        }
+    }
+}
---
+++ tests/Fields/FieldTest.php
@@ -0,0 +1,120 @@
+<?php namespace PascalKleindienst\FormListGenerator\Fields;
+
+/**
+ * Field Test Class
+ * @package \PascalKleindienst\FormListGenerator\Fields
+ */
+class FieldTest extends \PHPUnit_Framework_TestCase
+{
+    protected function createField($name, $config = [])
+    {
+        $field = new Field($name, $config);
+        $field->setup();
+        
+        return $field;
+    }
+
+    /* public function testConfigKeys()
+    {
+        $field = $this->createField('test-options', ['options' => 'Some opts']);
+        $this->assertEquals('Some opts', $field->options);
+    }
+
+    public function testSetup()
+    {
+        $field = $this->createField('test-cssClass', []);
+        $this->assertEquals(' ', $field->cssClass);
+    } */
+
+
+    /**
+     * @dataProvider provider
+     */
+    public function testGetValue($record, $config, $expected)
+    {
+        $field = $this->createField('record', $config);
+        $value = (string)$field->getValue($record);
+        $this->assertEquals($expected, $value);
+    }
+
+    public function provider()
+    {
+        return [
+            'default input' => [
+                ['record' => 'Value'],
+                [ 'label' => 'Label' ],
+                '<label class="d-block">Label<input type="text" name="record" value="Value" class=" form-control"></label>'
+            ],
+            'default input with default value' => [
+                ['record' => null],
+                [ 'label' => 'Label', 'default' => 'Default' ],
+                '<label class="d-block">Label<input type="text" name="record" value="Default" class=" form-control"></label>'
+            ],
+            'disabled input' => [
+                ['record' => 'Value'],
+                [ 'label' => 'Label', 'disabled' => true ],
+                '<label class="d-block">Label<input type="text" name="record" value="Value" disabled="disabled" class=" form-control"></label>'
+            ],
+            'readonly input' => [
+                ['record' => 'Value'],
+                [ 'label' => 'Label',  'readOnly' => true ],
+                '<label class="d-block">Label<input type="text" name="record" value="Value" readonly="1" class=" form-control"></label>'
+            ],
+            'required input' => [
+                ['record' => 'Value'],
+                [ 'label' => 'Label', 'required' => true ],
+                '<label class="d-block">Label *<input type="text" name="record" value="Value" required="required" class=" form-control"></label>'
+            ],
+            'css input' => [
+                ['record' => 'Value'],
+                [ 'label' => 'Label', 'cssClass' => 'csstest' ],
+                '<label class="d-block">Label<input type="text" name="record" value="Value" class="csstest form-control"></label>'
+            ],
+            'placeholder input' => [
+                ['record' => 'Value'],
+                [ 'label' => 'Label', 'placeholder' => 'placeholder test' ],
+                '<label class="d-block">Label<input type="text" name="record" value="Value" placeholder="placeholder test" class=" form-control"></label>'
+            ],
+            'comment input' => [
+                ['record' => 'Value'],
+                [ 'label' => 'Label', 'comment' => 'Input with comment' ],
+                '<label class="d-block">Label<label class="d-block  "><input type="text" name="record" value="Value"> Input with comment</label></label>'
+            ],
+            'password input' => [
+                ['record' => 'Value'],
+                [ 'label' => 'Label', 'type' => 'password' ],
+                '<label class="d-block">Label<input type="password" name="record" value="Value" class=" form-control"></label>'
+            ],
+            'date input' => [
+                ['record' => '2017-05-04'],
+                [ 'label' => 'Label', 'type' => 'date' ],
+                '<label class="d-block">Label<input type="date" name="record" value="2017-05-04" class=" form-control"></label>'
+            ],
+            'email input' => [
+                ['record' => 'Value'],
+                [ 'label' => 'Label', 'type' => 'email' ],
+                '<label class="d-block">Label<input type="email" name="record" value="Value" class=" form-control"></label>'
+            ],
+            'textarea input' => [
+                ['record' => 'Value'],
+                [ 'label' => 'Label', 'type' => 'textarea' ],
+                '<label class="d-block">Label<textarea name="record" rows="10" cols="50" class=" form-control">Value</textarea></label>'
+            ],
+            'dropdown input' => [
+                ['record' => 2],
+                [ 'label' => 'Label', 'type' => 'dropdown', 'options' => [1=>1, 2=>2] ],
+                '<label class="d-block">Label<select name="record" class=" form-control"><option value="1">1</option><option value="2" selected>2</option></select></label>'
+            ],
+            'checkbox input' => [
+                ['record' => 'Value'],
+                [ 'label' => 'Label', 'type' => 'checkbox' ],
+                '<label class="d-block">Label<input type="checkbox" name="record" value="Value" class=" form-control"></label>'
+            ],
+            'checkbox input default' => [
+                ['record' => 'Value'],
+                [ 'label' => 'Label', 'type' => 'checkbox', 'default' => true],
+                '<label class="d-block">Label<input type="checkbox" name="record" value="Value" checked="checked" class=" form-control"></label>'
+            ],
+        ];
+    }
+}
