2af6f6a9ff23c7c5aa5216883b41b80d684f78f3
Pascal Kleindienst <mail@pascalkleindienst.de>
2017-07-17 13:45:02 +0200

    updated test for translation

--- tests/Generators/FormGeneratorTest.php
+++ tests/Generators/FormGeneratorTest.php
@@ -21,14 +21,43 @@ class FormGeneratorTest extends \PHPUnit_Framework_TestCase
         $this->assertInstanceOf(\PascalKleindienst\FormListGenerator\Fields\FieldFactory::class, $this->generator->getFactory());
     }
 
+    /**
+     * @runInSeparateProcess
+     * @preserveGlobalState disabled
+     */
     public function testRender()
     {
         ob_start();
+        date_default_timezone_set('Europe/Berlin');
         $this->generator->render(['test' => 'Testing']);
         $this->assertEquals(
             '    <div class="form-group col-md-6">' . PHP_EOL .
-            '        <label class="d-block">Test<input type="text" name="test" value="Testing" class=" form-control"></label>' .
+            '        <label class="d-block">Test<label class="d-block  "><input type="text" name="test" value="Testing" placeholder="Some Placeholder"> Some Comment</label></label>' .
             '                    <small class="form-text text-muted">Some Description</small>'  . PHP_EOL .
+            '            </div>' . PHP_EOL,
+            ob_get_contents()
+        );
+        ob_end_clean();
+    }
+
+    /**
+     * @runInSeparateProcess
+     * @preserveGlobalState disabled
+     */
+    public function testRenderTranslated()
+    {
+        ob_start();
+        date_default_timezone_set('Europe/Berlin');
+
+        $this->generator->setTranslator(function ($message) {
+            return 'Translated: ' . $message;
+        });
+        $this->generator->render(['test' => 'Testing']);
+        $this->assertEquals(
+            '    <div class="form-group col-md-6">' . PHP_EOL .
+            '        <label class="d-block">Translated: Test<label class="d-block  "><input type="text" name="test" value="Testing" placeholder="Translated: Some Placeholder">' .
+            ' Translated: Some Comment</label></label>' .
+            '                    <small class="form-text text-muted">Translated: Some Description</small>'  . PHP_EOL .
             '            </div>' . PHP_EOL,
             ob_get_contents()
         );
